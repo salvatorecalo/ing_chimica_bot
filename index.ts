2a836c9eef8776b6db28a54b6a75b29275ab68ea
Salvo <dafder45678@gmail.com>
2024-09-01 14:44:38 +0200

    fix: file import

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 import "./bot"; // questo avvierà il bot
-import './utils/keyboard/keyboard';  // Assicurati che questo file venga importato dopo il bot
+import './utils/keyboard/keyboards'
 import './inlineQueries'; // Importa la gestione delle query inline
